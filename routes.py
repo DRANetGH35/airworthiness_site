cbfe6378f7ad582b45777833c1d72e15e438c0b9
Grok <devanaptaker@gmail.com>
2026-05-19 10:33:12 -0700

    add: home and register page

--- routes.py
+++ routes.py
@@ -9,3 +9,7 @@ app = create_app()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')

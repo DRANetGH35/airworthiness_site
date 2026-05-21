5a61c39ae6056805dca973951afde03c443cbedb
Grok <devanaptaker@gmail.com>
2026-05-20 20:10:56 -0700

    add: Plane table

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from typing import List
 from sqlalchemy import Integer, String, Boolean, DateTime, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 import datetime
@@ -14,6 +15,7 @@ class User(UserMixin, db.Model):
     email: Mapped[str] = mapped_column(String(1000))
     is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False)
     verified: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    planes: Mapped[List["Plane"]] = relationship(back_populates="user")
 
     @classmethod
     def exists(cls, username: str) -> bool:
@@ -27,3 +29,10 @@ class User(UserMixin, db.Model):
     def get(cls, user_id):
         return cls.query.filter_by(id=user_id).first()
 
+class Plane(db.Model):
+    __tablename__ = "plane_table"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(1000))
+    user: Mapped[User] = relationship("User", back_populates="planes")
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user_table.id"))

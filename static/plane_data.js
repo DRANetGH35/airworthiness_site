4242d8004d478619b3faeba8bd748cdc05ee7660
Grok <devanaptaker@gmail.com>
2026-06-04 12:16:55 -0700

    add: maintenance table items turn yellow when 25% until due

--- static/plane_data.js
+++ static/plane_data.js
@@ -6,24 +6,71 @@ const maintenance_table = document.getElementById('maintenance_table')
 const planeField = document.getElementById("plane")
 const engineField = document.getElementById("engine")
 
-function colorTableOverdue(){
+function IterateTable(){
     for (let i = 2; i < maintenance_table.rows.length; i++){
         row = maintenance_table.rows[i]
         type = row.dataset.maintenance_type
-        time_due = row.children[2].innerHTML
-        if (type == "engine"){
-            if (time_due < engineTime()){
+        time_due = row.children[2].innerHTML != "---" ?  parseFloat(row.children[2].innerHTML) : null
+        date_due = row.children[1].innerHTML != "---" ?  row.children[1].innerHTML : null
+        if (date_due){
+            date_due = new Date(date_due);
+            todays_date = new Date();
+            if (date_due < todays_date){
+                console.log('date_overdue')
                 row.classList.add('table-danger')
+            }
+        }
+        if (type == "engine"){
+            if (time_due && time_due < engineTime()){
+                row.classList.add('table-danger')
+            }
+            if(AlmostDue(row, 'engine')){
+                row.classList.add('table-warning')
             }
         }
         if (type == "aircraft"){
-            if (time_due < planeTime()){
+            if (time_due && time_due < planeTime()){
                 row.classList.add('table-danger')
             }
+            if (AlmostDue(row, 'aircraft')){
+                row.classList.add('table-warning')
+            }
         }
+
     }
 }
 
+function AlmostDue(row, maintenance_type){
+    if (row.dataset.interval) {
+            today = new Date();
+            date_due = row.children[1].innerHTML != "---" ?  new Date(row.children[1].innerHTML) : null
+            current_time = time_due = row.children[2].innerHTML != "---" ?  parseFloat(row.children[2].innerHTML) : null
+            //set total hours to hours recorded on engine or aircraft depending on row
+            total_hours = maintenance_type == "engine" ? engineTime() : planeTime()
+            interval_hours = row.dataset.interval_hours != "" ? parseFloat(row.dataset.interval_hours): null;
+            interval_months = row.dataset.interval_months != "" ? parseFloat(row.dataset.interval_months): null;
+            if (interval_hours){
+                tach_hours_till_due = current_time - total_hours
+                percentage_complete = ((tach_hours_till_due / interval_hours) * 100)
+                if (((tach_hours_till_due / interval_hours) * 100) < 25){
+                    return true
+                }
+            }
+            if (interval_months){
+                ms_till_due = date_due - today
+                s_till_due = Math.floor(ms_till_due / 1000)
+                min_till_due = Math.floor(s_till_due / 60)
+                hr_till_due = Math.floor(min_till_due / 60)
+                d_till_due = Math.floor(hr_till_due / 24)
+                months_till_due = Math.floor(d_till_due / 30)
+                percentage_until_due = 100 - ((d_till_due / (interval_months * 30)) * 100)
+                if (percentage_until_due > 25){
+                    return true
+                }
+            }
+        }
+    return false
+}
 
 function planeTime(){
     return parseFloat(planeField.innerHTML.split(":")[1].split(" ")[1])
@@ -54,4 +101,4 @@ tachTimeForm.addEventListener('submit', function (event) {
         event.preventDefault();
     }
 })
-colorTableOverdue();
+IterateTable();

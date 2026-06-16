e9a2936a771fca54fff50752676cbd88971d4913
Grok <devanaptaker@gmail.com>
2026-06-16 08:52:39 -0700

    change: showEditFields argument name change

--- static/edit_entry.js
+++ static/edit_entry.js
@@ -27,8 +27,8 @@ function cancelEditField(id){
     save_cell.classList.add('hidden')
 }
 
-function showEditFields(time_entry_id){
-    let id_selector = `time_entry_id_${time_entry_id}`
+function showEditFields(id){
+    let id_selector = `time_entry_id_${id}`
     let row = document.getElementById(id_selector)
     let tach_time = row.children[1].children[0]
     let tach_entry = row.children[1].children[1]

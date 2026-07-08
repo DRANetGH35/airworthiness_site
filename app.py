7a8ee9e37192f6f36085d584e67dd0d30aa0b9d5
Grok <devanaptaker@gmail.com>
2026-07-07 19:34:45 -0700

    fix app config settings to allow external url_for use

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Time the user out after 30 minutes
+    app.config['SERVER_NAME'] = 'airworthiness.dradigital.net'
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.config['APPLICATION_ROOT'] = '/'
+    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['REMEMBER_COOKIE_SECURE'] = True
+
+    from werkzeug.middleware.proxy_fix import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     db.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
